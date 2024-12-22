3390789a9fed99035d90533ac2695f175fef6fb5
hpnightowl <iamhp2k@gmail.com>
2024-12-22 06:53:20 +0000

    vendor: guard qcom configurations

--- configs/BoardConfig404.mk
+++ configs/BoardConfig404.mk
@@ -12,8 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+ifeq ($(call is-board-platform-in-list,$(QCOM_BOARD_PLATFORMS)),true)
 # Include our Qualcomm Board configuration.
 include device/qcom/common/BoardConfigQcom.mk
+endif
 
 # Kernel
 include vendor/404/configs/BoardConfigKernel.mk

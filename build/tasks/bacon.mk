40dab47a97b6ba6b19d3ba089517e4b7faf06b1e
LuK1337 <priv.luk@gmail.com>
2024-12-28 11:30:46 +0000

    404: bacon: Depend on $(DEFAULT_GOAL) This makes sure that $OUT contains images for all partitions.
    Test: breakfast tangorpro && m bacon && ls $OUT/vendor_boot.img
    Change-Id: Ia59a1fb7d9d1054040453ff2e7954ea6d7c61fde
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: xenxynon <flynryder427@gmail.com>

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -20,7 +20,7 @@ P404_TARGET_PACKAGE := $(PRODUCT_OUT)/project-404-$(P404_VERSION).zip
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
 .PHONY: bacon
-bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
+bacon: $(DEFAULT_GOAL) $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(P404_TARGET_PACKAGE)
 	$(hide) $(SHA256) $(P404_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(P404_TARGET_PACKAGE).sha256sum
 	@echo -e ""
